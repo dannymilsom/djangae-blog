f0ab1bee5680d0ffc43e2c986e06cf684940c4d6
Adam Alton <adamalton@gmail.com>
2014-11-11 21:05:58 +0000

    Lock down Django admin with `login:admin` in app.yaml in addition to the inbuilt `is_staff` restriction.

--- scaffold/urls.py
+++ scaffold/urls.py
@@ -8,5 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'scaffold.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^_ah/', include('djangae.urls')),
+
+    # Note that by default this is also locked down with login:admin in app.yaml
     url(r'^admin/', include(admin.site.urls)),
 )
